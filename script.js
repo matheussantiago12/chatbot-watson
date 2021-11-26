647e1adc02ea4c72f24b3d22ac39842cd1f69cc0
Matheus Santiago <matheus.santiago@premiersoft.net>
2021-11-26 18:50:33 -0300

    finalizando chatbot

---
+++ script.js
@@ -0,0 +1,19 @@
+window.watsonAssistantChatOptions = {
+    integrationID: "1f48462e-e0c7-4b64-9925-e1fec8b32ed5",
+    region: "us-south",
+    serviceInstanceID: "b559a98b-e507-4125-bf2e-510ee2b88e3d",
+    onLoad: function(instance) {
+      const customLanguagePack = {
+          "input_placeholder": "Escreva algo...",
+          "homeScreen_conversationStarterLabel": "Comece agora",
+      };
+
+      instance.updateLanguagePack(customLanguagePack);
+      instance.render(); 
+    }   
+  };
+  setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+  document.head.appendChild(t);
+});
--- server.js
+++
@@ -1,4 +0,0 @@
-import express from 'express'
-import { app } from './src/app.js'
-
-app.listen(3333, () => console.log('Server is running at port 3333'))
