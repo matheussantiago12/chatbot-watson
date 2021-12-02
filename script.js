e38a5a26f27e2ca7c423a82c57a21a1ab775e61f
Matheus Santiago <matheus.santiago@premiersoft.net>
2021-12-02 18:39:18 -0300

    melhorando script

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ window.watsonAssistantChatOptions = {
       instance.updateLanguagePack(customLanguagePack);
       instance.render(); 
     }   
-  };
-  setTimeout(function(){
-  const t=document.createElement('script');
-  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
-  document.head.appendChild(t);
+};
+
+setTimeout(function(){
+  const script = document.createElement('script');
+  script.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+  document.head.appendChild(script);
 });
